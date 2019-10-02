336a607c25c08ee0621868723542985ab7950c81
Bessy Gee <nakiwarai13@gmail.com>
2019-10-02 13:04:25 -0400

    added gameplay functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -37,3 +37,44 @@ let hasFinished = false;
 //Amount of wins the player has scored
 let wins = 0;
 
+//Resets game stats
+function resetGame() {
+    remainingGuesses = maxGuesses;
+    gameStarted = false;
+}
+
+//Round random number down to whole
+currentTitleIndex = Math.floor(Math.random() * (animeTitles.lenth));
+
+//Clears the arrays
+guessedLetters = [];
+guessingTitle = [];
+
+//Building of the title being guessed and clears it out
+for (var i = 0; i < animeTitles[currentTitleIndex].length; i++) {
+    guessingTitle.push("_");
+}
+
+//Hides game over or win images/text
+document.getElementById("tryAgain").style.cssText = "display: none";
+document.getElementById("gameOverImg").style.cssText = "display: none";
+document.getElementById("youWinImg ").style.cssText = "display: none";
+
+//To update display on the HTML
+function updateDisplay() {
+    document.getElementById("totalWin").innerText = wins;
+    document.getElementById("currentTitle").innerText = "";
+    for (var i = 0; i < guessingTitle.lenth; i++) {
+        document.getElementById("currentTitle").innerText += guessingTitle[i];
+    }
+    document.getElementById("remainingGuesses").innerText = remainingGuesses;
+    document.getElementById("guessedLetters").innerText = guessedLetters;
+    if(remainingGuesses <= 0) {
+        document.getElementById("gameOverImg").style.cssText = "display: block";
+        document.getElementById("tryAgain").style.cssText = "display: blcok";
+        hasFinished = true;
+    }
+};
+
+//Display
+updateDisplay();
