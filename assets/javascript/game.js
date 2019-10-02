f595873e8a29396b29b3817011eef41f75642d11
Bessy Gee <nakiwarai13@gmail.com>
2019-10-02 12:48:43 -0400

    created global data

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+//List of anime titles to choose from
+let animeTitles = [
+    "Naruto",
+    "Pokemon",
+    "Attack on Titan",
+    "Rise of the Shield Hero",
+    "One Piece",
+    "Sailor Moon",
+    "Made in Abyss",
+    "My Hero Academia",
+    "Fullmetal Alchemist",
+    "Dragon Ball Z",
+    "Samurai Champloo",
+];
+
+//Maximum number of guesses
+const maxGuesses = 10;
+
+//Letters guessed by the player
+let guessedLetters = [];
+
+//Index of current title in the array
+let currentTitleIndex;
+
+//Title to build current title
+let guessingTitle = [];
+
+//The amount of guesses the player has left
+let remainingGuesses = 0;
+
+//To tell if game has started
+let gameStarted = false;
+
+//For "Press any key to try again"
+let hasFinished = false;
+
+//Amount of wins the player has scored
+let wins = 0;
+
