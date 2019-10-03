11a2e2127be778f2c8196be1d7e6032e30751d5e
Bessy Gee <nakiwarai13@gmail.com>
2019-10-03 12:04:26 -0400

    added function to update display and game over screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,9 @@
 //List of anime titles to choose from
 let animeTitles = [
     "Naruto",
-    "Pokemon",
+    "Neon Genesis Evangelion",
     "Attack on Titan",
-    "Rise of the Shield Hero",
+    "The Rising of the Shield Hero",
     "One Piece",
     "Sailor Moon",
     "Made in Abyss",
@@ -11,7 +11,10 @@ let animeTitles = [
     "Fullmetal Alchemist",
     "Dragon Ball Z",
     "Samurai Champloo",
+    "Goblin Slayer",
 ];
+
+console.log(animeTitles);
 
 //Maximum number of guesses
 const maxGuesses = 10;
@@ -28,24 +31,41 @@ let guessingTitle = [];
 //The amount of guesses the player has left
 let remainingGuesses = 0;
 
-//To tell if game has started
-let gameStarted = false;
-
-//For "Press any key to try again"
-let hasFinished = false;
-
 //Amount of wins the player has scored
 let wins = 0;
 
-//Resets game stats
-function resetGame() {
-    remainingGuesses = maxGuesses;
-    gameStarted = false;
-    document.getElementById("animeQuestion").style.cssText = "display:inline";
-}
-
-
-//Round random number down to whole
+//Selects random index in the array
 currentTitleIndex = Math.floor(Math.random() * (animeTitles.length));
 
+//Pushes "_" for every letter of random index in the array
+for (var i = 0; i < animeTitles[currentTitleIndex].length; i++) {
+    guessingTitle.push(" _");
+}
+
 console.log(currentTitleIndex);
+console.log(guessingTitle);
+
+//function to update the display on the HTML document
+function updateDisplay () {
+    document.getElementById("totalWin").innerText = wins;
+    document.getElementById("currentTitle").innerText = "";
+    for (var i =0; i < animeTitles[currentTitleIndex].length; i++) {
+        document.getElementById("currentTitle").innerText += guessingTitle[i];
+    }
+    document.getElementById("remainingGuesses").innerText = remainingGuesses;
+    document.getElementById("guessedLetters").innerText =  guessedLetters;
+    if (remainingGuesses <= 0) {
+        document.getElementById("tryAgain").style.cssText = "display: block";
+    }
+}
+
+//Game Over screen
+function gameOver() {
+    remainingGuesses = maxGuesses;
+    document.getElementById("animeQuestion").style.cssText = "display: none";
+    document.getElementById("pressKey").style.cssText = "display: block";
+    document.getElementById("tryAgain").style.cssText= "display: block";
+    document.getElementById("gameOverImg").style.cssText = "display: block";
+    document.getElementById("youWinImg").style.cssText = "display: none";
+}
+
